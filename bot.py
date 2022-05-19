e226081d97b5fda6d88fb790003c1ba6515699df
medvedarina12 <school.medvid@gmail.com>
2022-05-19 03:52:54 +0300

    dummy bot

---
+++ bot.py
@@ -0,0 +1,87 @@
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import Updater, CommandHandler
+from telegram.ext import MessageHandler, Filters, InlineQueryHandler
+import time
+
+
+TOKEN = '5303804790:AAHqFYXCn1WAmry3l0LueSyAWQ-8GMhylhQ'
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
+
+# функция обработки команды '/start'
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, 
+                             text="Привет,я бот  ты можешь  говорить со мной ")
+
+# функция обработки текстовых сообщений
+def echo(update, context):
+    text = 'я не могу это зделать ' + update.message.text 
+    if update.message.text == 'Мне скучно':
+        text = "Можешь сыграть в эту игру https://play.google.com/store/apps/details?id=com.digidust.elokence.akinator.freemium&hl=en_US " 
+    if update.message.text == 'Включи песню':       
+        text = "Вот плейлист https://www.youtube.com/watch?v=UtPHhuIj8f8" 
+    if update.message.text == 'Кто выграл евровиденье 2022?':       
+        text = "Украина  выграла евровиденье 2022 "     
+    if update.message.text == 'Кто выграет войну между РФ и Украиной?':       
+        text = "Украина  "
+    if update.message.text == 'Слава Украине!':       
+        text = "Героям Слава!"
+    context.bot.send_message(chat_id=update.effective_chat.id, 
+                             text=text)    
+
+# функция обработки команды '/caps'
+def caps(update, context):
+    if context.args:
+        text_caps = ' '.join(context.args).upper()
+        context.bot.send_message(chat_id=update.effective_chat.id, 
+                                text=text_caps)
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, 
+                                text='No command argument')
+        context.bot.send_message(chat_id=update.effective_chat.id, 
+                                text='send: /caps argument')
+
+# функция обработки встроенного запроса
+def inline_caps(update, context):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = list()
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Convert to UPPER TEXT',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+# функция обработки не распознных команд
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, 
+                             text="Sorry, I didn't understand that command.")
+
+# обработчик команды '/start'
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)    
+
+# обработчик текстовых сообщений
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+# обработчик команды '/caps'
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+# обработчик встроенных запросов 
+inline_caps_handler = InlineQueryHandler(inline_caps)
+dispatcher.add_handler(inline_caps_handler)
+
+# обработчик не распознных команд
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
+# запуск прослушивания сообщений
+updater.start_polling()
+# обработчик нажатия Ctrl+C
+updater.idle()
