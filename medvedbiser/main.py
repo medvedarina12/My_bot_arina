040039877090e9de05c9d635fcb29b719c5badbc
medvedarina12 <school.medvid@gmail.com>
2022-06-25 12:00:12 +0300

    shop

--- medvedbiser/main.py
+++ medvedbiser/main.py
@@ -12,17 +12,21 @@ def start_message(message):
     MypyBot.send_message(
         message.chat.id,
         """
-            Доброго дня, Вас вітає інтернет магазин прикрас:
-            Medved_biser.ua
-            https://www.instagram.com/medved_biser.ua/
-            Подивитись посилання на актуальні новини нашого магазину:
-            /links
+            Доброго дня,
+Вас вітає інтернет магазин прикрас:
+ℳedved_biser.uα
+Подивитись актуальні новини нашого магазину❤:
+/relevant
+Ознайомитись з магазином можна за посиланням:
+https://www.instagram.com/medved_biser.ua/
+Замовити прикраси 🛍 ↓
+/by
         """,
         reply_markup=types.ReplyKeyboardRemove()    # убираем клавиатуру если была
     )
 
 
-@MypyBot.message_handler(commands=['links'])
+@MypyBot.message_handler(commands=['relevant'])
 def button_message(message):
     markup=types.InlineKeyboardMarkup()
     item1=types.InlineKeyboardButton(
@@ -34,6 +38,16 @@ def button_message(message):
         url='https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTA5OTc3ODYyMzkxNTk1?igshid=YmMyMTA2M2Y='
     )
     markup.add(item1, item2)
+    MypyBot.send_message(message.chat.id,'💁Актуальні новинки нашого магазина  тут ↓',reply_markup=markup)
+
+
+@MypyBot.message_handler(commands=['by'])
+def button_pmenu(message):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    item1=types.KeyboardButton("В інсті")
+    item2=types.KeyboardButton("В телеграм боті")
+    markup.add(item1, item2)
+    MypyBot.send_message(message.chat.id,'Де Вам зручніше оформити замовлення?',reply_markup=markup)
 
 
 # отладочная функция для отслеживания пользователей
