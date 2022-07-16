af7f533566e49e8c09c99f2cd908b0f8b146a97f
medvedarina12 <school.medvid@gmail.com>
2022-07-16 13:06:22 +0300

    catalog

--- medvedbiser/db_methods.py
+++ medvedbiser/db_methods.py
@@ -2,7 +2,7 @@ import sqlite3
 import time
 import datetime
 
-from settings import users, GLOBAL_PATH
+from settings import users, GLOBAL_PATH, order_template
 
 
 def save_data(message):  
@@ -12,34 +12,31 @@ def save_data(message):
     connection = sqlite3.connect(GLOBAL_PATH + 'mb_data.db')
     c = connection.cursor()
     c.execute(
-        f'CREATE TABLE IF NOT EXISTS {table_name}( title TEXT, size TEXT, amount TEXT, phone TEXT, destination TEXT, name TEXT, payment TEXT, datestamp TEXT, unix TEXT )'
+        f'CREATE TABLE IF NOT EXISTS {table_name}( title TEXT, size TEXT, amount TEXT, phone TEXT, destination TEXT, name TEXT, payment TEXT, datestamp TEXT, unix REAL )'
     )
     unix = time.time()
     date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
+    data_val = list(users[u_id]['order_values'].values())
+    data_val.extend([date, unix])
     c.execute(
         f"INSERT INTO {table_name} (title, size, amount, phone, destination, name, payment, datestamp, unix) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", 
-        (
-            users[u_id]['order']['q1'], 
-            users[u_id]['order']['q2'], 
-            users[u_id]['order']['q3'], 
-            users[u_id]['order']['q4'],
-            users[u_id]['order']['q5'],
-            users[u_id]['order']['q6'],
-            users[u_id]['order']['q7'],
-            date, unix
-        )
+        data_val
     )
     connection.commit()
     c.close()
     connection.close()
 
 
-def get_data(user_name_id):
+def get_data(message):
+    user_name_id = message.from_user.first_name + '_' \
+        + message.from_user.last_name + '_' + str(message.chat.id)
     connection = sqlite3.connect(GLOBAL_PATH + 'mb_data.db')
     c = connection.cursor()
     c.execute(f"SELECT * FROM {user_name_id}")
     data = c.fetchall()
-    print(data)
     c.close()
     connection.close()
-    return f"Заказ {user_name_id}, {data}"
+    return order_template.format(
+        user_name_id, 
+        data[-1]
+    )
--- medvedbiser/main.py
+++ medvedbiser/main.py
@@ -1,6 +1,7 @@
 from telebot import types
 
-from settings import MypyBot, users, legend, q_base, ROOT_ID
+from helpers import *
+from settings import legend, ROOT_ID
 from db_methods import save_data, get_data
 
 
@@ -10,30 +11,45 @@ from db_methods import save_data, get_data
 
 @MypyBot.message_handler(commands=['start', 'help'])
 def start_message(message):
-    MypyBot.send_message(
-        message.chat.id,
-        legend,
-        reply_markup=types.ReplyKeyboardRemove()
-    )
+    MypyBot.send_message(message.chat.id, legend, reply_markup=types.ReplyKeyboardRemove())
 
 
 @MypyBot.message_handler(commands=['links'])
 def button_message(message):
-    markup=types.InlineKeyboardMarkup()
-    item1=types.InlineKeyboardButton(
-        "Відгуки", 
+    markup = types.InlineKeyboardMarkup()
+    item1 = types.InlineKeyboardButton(
+        "Відгуки",
         url='https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTIyMTAyMzk4MzExODAx?igshid=YmMyMTA2M2Y='
     )
-    item2=types.InlineKeyboardButton(
-        "Ukrainian collection", 
+    item2 = types.InlineKeyboardButton(
+        "Ukrainian collection",
         url='https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTA5OTc3ODYyMzkxNTk1?igshid=YmMyMTA2M2Y='
     )
     markup.add(item1, item2)
-    MypyBot.send_message(message.chat.id,'Актуальні новини магазину: ',reply_markup=markup)
+    MypyBot.send_message(
+        message.chat.id, 'Актуальні новини магазину: ', reply_markup=markup)
+
+
+@MypyBot.message_handler(commands=['catalog'])
+def show_catalog(message):
+    
+    # functionality in progress ...
+    
+    markup = types.InlineKeyboardMarkup()
+    btn_link1 = types.InlineKeyboardButton(
+        "...",
+        url='https://...'
+    )
+    btn_link2 = types.InlineKeyboardButton(
+        "...",
+        url='https://...'
+    )
+    markup.add(btn_link1, btn_link2)
+    MypyBot.send_message(message.chat.id, 'Каталог: ', reply_markup=markup)
 
 
 @MypyBot.message_handler(commands=['find'])
-def yourCommand(message):
+def find_item(message):
     status: list = extract_arg(message.text)
     print(status)
 
@@ -57,16 +73,8 @@ def by_item(message):
         users[str(message.chat.id)] = {
             'd_checker': False,
             'd_cnt': 0,
-            'order': {
-                'status': False,
-                'q1': False,
-                'q2': False,
-                'q3': False,
-                'q4': False,
-                'q5': False,
-                'q6': False,
-                'q7': False,
-            }
+            'order_status': False,
+            'order_values': dict(('q' + str(i), False) for i in range(1, len(q_base)+1))
         }
     users[str(message.chat.id)]['d_checker'] = True
     MypyBot.send_message(
@@ -81,60 +89,26 @@ def cancel_dialog(message):
     if not users:
         MypyBot.send_message(message.chat.id, 'without cancellation')
     else:
-        users[str(message.chat.id)]['d_checker'] = False
-        users[str(message.chat.id)]['d_cnt'] = 0
-        users[str(message.chat.id)]['order']['status'] = False
+        clear_user(message.chat.id)
         MypyBot.send_message(message.chat.id, "Придбання товару відмінено!")
 
-
-#--------------------#
-#  HELPER FUNCTIONS  #
-#--------------------#
-
-def extract_arg(arg) -> list:
-    return arg.split()[1:]
-
-
-def dialogQuestion(msg):
-    qcnt = 'q' + str(users[str(msg.chat.id)]['d_cnt'])      # q1 q8 
-    q = q_base[qcnt]
-    MypyBot.send_message(
-        msg.chat.id, 
-        q, 
-    )
-
-
-def temp_save_ansvers(msg):
-    if msg.text == '/by':
-        users[str(msg.chat.id)]['order']['status'] = True
-    else:
-        cnt = str(users[str(msg.chat.id)]['d_cnt'])
-        users[str(msg.chat.id)]['order']['q' + cnt] = msg.text
 
 #--------------------#
 # REPLYER FUNCTIONS  #
 #--------------------#
 
-@MypyBot.message_handler(content_types = ['text'])
+@MypyBot.message_handler(content_types=['text'])
 def replyer(message):
-   if users:
-        if users and users[str(message.chat.id)]['d_checker']:
+    if str(message.chat.id) in users:
+        if users[str(message.chat.id)]['d_checker']:
             temp_save_ansvers(message)
             if users[str(message.chat.id)]['d_cnt'] < len(q_base):
                 dialogQuestion(message)
                 users[str(message.chat.id)]['d_cnt'] += 1
             else:
                 save_data(message)
-                MypyBot.send_message(
-                    ROOT_ID,
-                    get_data(
-                        message.from_user.first_name + '_' \
-                         + message.from_user.last_name + '_' \
-                          + str(message.chat.id)
-                    )
-                )
-                users[str(message.chat.id)]['d_checker'] = False
-                users[str(message.chat.id)]['d_cnt'] = 0
+                MypyBot.send_message(ROOT_ID, get_data(message))
+                clear_user(message.chat.id)
                 MypyBot.send_message(
                     message.chat.id,
                     "Ваше замовлення передано в обробку, менеджер зв'яжеться з Вами для підтвердження замовлення. Дякуємо за співпрацю."
